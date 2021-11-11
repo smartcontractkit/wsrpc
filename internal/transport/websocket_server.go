904eb5e8f9f7ca801f014c781d2b08839afe7caa
Makram Kamaleddine <makramkd@users.noreply.github.com>
2021-11-11 16:31:06 -0500

    Fix ping handler on WS server

--- internal/transport/websocket_server.go
+++ internal/transport/websocket_server.go
@@ -1,6 +1,7 @@
 package transport
 
 import (
+	"net"
 	"sync"
 	"time"
 
@@ -101,6 +102,20 @@ func (s *WebsocketServer) start() {
 	s.writePump()
 }
 
+func (s *WebsocketServer) pingHandler(message string) error {
+	if err := s.conn.SetReadDeadline(time.Now().Add(pongWait)); err != nil {
+		return err
+	}
+
+	err := s.conn.WriteControl(websocket.PongMessage, []byte(message), time.Now().Add(time.Second))
+	if err == websocket.ErrCloseSent {
+		return nil
+	} else if e, ok := err.(net.Error); ok && e.Temporary() {
+		return nil
+	}
+	return err
+}
+
 // readPump pumps messages from the websocket connection.
 //
 // The application runs readPump in a per-connection goroutine. The application
@@ -111,13 +126,7 @@ func (s *WebsocketServer) readPump() {
 		defer close(s.done)
 	}()
 
-	s.conn.SetPingHandler(func(string) error {
-		if err := s.conn.SetReadDeadline(time.Now().Add(pongWait)); err != nil {
-			return err
-		}
-
-		return nil
-	})
+	s.conn.SetPingHandler(s.pingHandler)
 
 	for {
 		_, message, err := s.conn.ReadMessage()
