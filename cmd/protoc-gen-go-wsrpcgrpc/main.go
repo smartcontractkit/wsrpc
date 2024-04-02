70e6c4d4b819d21a55162b7850882bf0591f1113
patrickhuie19 <patrick.huie@smartcontract.com>
2024-04-01 23:51:33 -0400

    Adding protoc-gen-go-wsrpcgrpc

---
+++ cmd/protoc-gen-go-wsrpcgrpc/main.go
@@ -0,0 +1,67 @@
+// protoc-gen-go-wsrpcgrpc is a plugin for the Google protocol buffer compiler to
+// generate Go code. Install it by building this program and making it
+// accessible within your PATH with the name:
+//
+//	protoc-gen-go-wsrpcgrpc
+//
+// The 'go-wsrpcgrpc' suffix becomes part of the argument for the protocol compiler,
+// such that it can be invoked as:
+//
+//	protoc --go-wsrpcgrpc_out=. path/to/file.proto
+//
+// This generates Go service definitions for the protocol buffer defined by
+// file.proto.  With that input, the output will be written to:
+//
+//	path/to/file_wsrpc_grpc.pb.go
+package main
+
+import (
+	"flag"
+	"fmt"
+
+	"google.golang.org/protobuf/compiler/protogen"
+	"google.golang.org/protobuf/types/pluginpb"
+)
+
+type strFlags []string
+
+func (f *strFlags) String() string {
+	return fmt.Sprintf("%s", *f)
+}
+
+func (f *strFlags) Set(value string) error {
+	*f = append(*f, value)
+
+	return nil
+}
+
+const version = "0.0.1"
+
+var clientServices strFlags
+
+func main() {
+	showVersion := flag.Bool("version", false, "print the version and exit")
+	flag.Parse()
+	if *showVersion {
+		fmt.Printf("protoc-gen-go-wsrpc %v\n", version)
+
+		return
+	}
+
+	var flags flag.FlagSet
+	flags.Var(&clientServices, "client-services", "Generate Client RPC services for these services")
+
+	protogen.Options{
+		ParamFunc: flags.Set,
+	}.Run(func(gen *protogen.Plugin) error {
+		gen.SupportedFeatures = uint64(pluginpb.CodeGeneratorResponse_FEATURE_PROTO3_OPTIONAL)
+		for _, f := range gen.Files {
+			if !f.Generate {
+				continue
+			}
+			generateFile(gen, f)
+		}
+
+		return nil
+	})
+}
